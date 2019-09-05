03e0814b239d68a937688340b76b7536a2c4e5ef
MaryKalantay <marykalantay@MaryKalantay.local>
2019-09-05 11:11:41 +0300

    Datapicker

---
+++ web-starter-kit-master/src/js/app.js
@@ -0,0 +1,17 @@
+// You can write a call and import your functions in this file.
+//
+// This file will be compiled into app.js and will not be minified.
+// Feel free with using ES6 here.
+
+import navBar from './modules/navbar';
+// import slickSlider from './modules/slick-slider';
+import rangeDatepicker from './modules/datepicker';
+
+(($) => {
+  // When DOM is ready
+  $(() => {
+    navBar.mobileMenu();
+    // slickSlider.slider();
+    rangeDatepicker.picker();
+  });
+})(jQuery);
