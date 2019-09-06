a363ee25025f82ec4b8f78da26d79ab1eab468cb
MaryKalantay <marykalantay@MaryKalantay.local>
2019-09-06 05:54:47 +0300

    Multi Slicks

--- web-starter-kit-master/src/js/modules/slick-slider.js
+++ web-starter-kit-master/src/js/modules/slick-slider.js
@@ -1,24 +1,40 @@
 // slick Slider
 const slickSlider = () => {
   const slider = () => {
-    $('.slick-slider').slick({
-      arrows: true,
-      slidesToShow: 1,
-      // nextArrow: '<svg><use xlink:href="#arr-r" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>',
-      // prevArrow: '<svg><use xlink:href="#arr-l" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>',
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          infinite: true,
-        },
-      }, {
-        breakpoint: 600,
-        settings: {
-          dots: true,
-          arrows: false,
-        },
-      }],
+    const sliders = [
+      {sslider : '#slider_1'},
+      {sslider : '#slider_2'},
+      {sslider : '#slider_3'},
+      {sslider : '#slider_4'},
+    ];
+    $.each(sliders, function() {
+      $(this.sslider).slick({
+        
+      });
     });
+
+
+
+
+
+    // $('.slick-slider').slick({
+    //   arrows: true,
+    //   slidesToShow: 1,
+    //   // nextArrow: '<svg><use xlink:href="#arr-r" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>',
+    //   // prevArrow: '<svg><use xlink:href="#arr-l" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>',
+    //   responsive: [{
+    //     breakpoint: 1024,
+    //     settings: {
+    //       infinite: true,
+    //     },
+    //   }, {
+    //     breakpoint: 600,
+    //     settings: {
+    //       dots: true,
+    //       arrows: false,
+    //     },
+    //   }],
+    // });
   };
   return {
     slider,
