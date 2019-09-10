bc0606cc0cf98f1637563bd6011eb463824dd882
artem.kalantai <artem.kalantai@centro.team>
2019-09-10 14:46:35 +0300

    new picker

--- web-starter-kit-master/src/js/modules/lightpick.js
+++ web-starter-kit-master/src/js/modules/lightpick.js
@@ -3,7 +3,79 @@ import moment from "moment";
 import 'moment/locale/ru';
 
 const rangeDatepicker = () => {
-  const picker = new Lightpick({ field: document.getElementById('datepicker') });
+  const picker = () => {
+    const dateFormat = 'dd. D MMM YYYY';
+
+    let selectedCheckInDate = null;
+    let selectedCheckOutDate = null;
+    const checkInDateInput = $('#checkIn');
+    const checkOutDateInput = $('#checkOut');
+
+    let checkOutPicker = null;
+
+    const checkInPicker = new Lightpick({
+      numberOfColumns: 2,
+      numberOfMonths: 2,
+      singleDate: true,
+      field: document.getElementById('checkIn'),
+      onSelect: function (start, end) {
+        console.log("selected: " + moment(start));
+        selectedCheckInDate = moment(start);
+        checkInDateInput.val(moment(start).locale('ru').format(dateFormat));
+        checkOutPicker = new Lightpick({
+          numberOfColumns: 2,
+          numberOfMonths: 2,
+          singleDate: false,
+          startDate: selectedCheckInDate,
+          field: document.getElementById('checkOut'),
+          onSelect: function (start, end) {
+            selectedCheckInDate = moment(start);
+            checkInDateInput.val(moment(start).locale('ru').format(dateFormat));
+            selectedCheckOutDate = moment(end);
+            checkOutDateInput.val(moment(end).locale('ru').format(dateFormat));
+          },
+
+          onOpen: function (start, end) {
+            checkOutDateInput.val('');
+          },
+        });
+        checkOutDateInput.focus();
+      },
+    });
+
+    checkOutDateInput.on('click', function () {
+      if (!checkOutPicker) {
+        checkOutPicker = checkOutPicker = new Lightpick({
+          numberOfColumns: 2,
+          numberOfMonths: 2,
+          singleDate: false,
+          format: ' ',
+          separator: ' ',
+          repick: true,
+          field: document.getElementById('checkOut'),
+          onSelect: function (start, end) {
+            selectedCheckInDate = moment(start);
+            checkInDateInput.val(moment(start).locale('ru').format(dateFormat));
+            checkInPicker.setDate(start);
+            if (end) {
+              selectedCheckOutDate = moment(end);
+              checkOutDateInput.val(moment(end).locale('ru').format(dateFormat));
+            } else {
+              setTimeout(function () {
+                checkOutDateInput.val('');
+              }, 0);
+            }
+          },
+
+          onOpen: function (start, end) {
+            checkOutDateInput.val('');
+          },
+        });
+      }
+      checkOutPicker.show();
+    })
+  };
+
   return {
     picker,
   };
