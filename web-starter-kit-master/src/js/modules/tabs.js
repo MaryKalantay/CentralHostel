964adac8b09db482d7ed6d7b3d58bfc05c4eb84c
artem.kalantai <artem.kalantai@centro.team>
2019-09-27 11:49:39 +0300

    tabs fix

--- web-starter-kit-master/src/js/modules/tabs.js
+++ web-starter-kit-master/src/js/modules/tabs.js
@@ -6,21 +6,26 @@ const Tabs = () => {
       1: { mtabs : '#tabs_1' },
       2: { mtabs : '#tabs_2' },
     };
-    $.each(multiTabs, function() {
-      let tabitem = $(this.mtabs).find('.tab-menu a');
-      let tabcontent = $(this.mtabs).find('.item-content');
-      console.log(this);
-      console.log(tabitem);
-      $(tabitem).click( function() {
-        $('.tab-menu a').removeClass('active');
+    $.each(multiTabs, function(tabIndex, tabValue) {
+      let tabItem = $(tabValue.mtabs).find('.tab-menu a');
+      $(tabItem).click( function() {
+        let currentTabId = $(this).attr('href');
+        let currentTabsBlockId = currentTabId.replace(new RegExp('(\\#tabs_\\d+)_\\d+'), '$1');
+        let currentTabContent = $(currentTabsBlockId + ' div .item-content.active');
+
+        //remove active class from active tab
+        $(currentTabsBlockId + ' a.active').removeClass('active');
+        //add active class to current tab
         $(this).addClass('active');
-        $(tabcontent).removeClass('active');
-        $(tabcontent).fadeOut('');
-        let activeTab = $(this).attr('href');
-        console.log(activeTab[1]);
-        $('.item-content[id=' + activeTab[1] + ']').addClass('active');
-        // console.log($(this).find('.item-content[id=' + activeTab[1] + ']'));
-        $(activeTab).fadeIn();
+
+        //remove active class from active tabContent
+        currentTabContent.removeClass('active');
+        currentTabContent.fadeOut('');
+
+        //add active class to current tabContent
+        $(currentTabId).addClass('active');
+        $(currentTabId).fadeIn();
+
         return false;
       });
     });
