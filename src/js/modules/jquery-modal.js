5e9215906ef75a1e24f293cd534ae0ced8e870d5
artem.kalantai <artem.kalantai@centro.team>
2019-10-22 09:19:52 +0300

    change video

--- src/js/modules/jquery-modal.js
+++ src/js/modules/jquery-modal.js
@@ -63,26 +63,6 @@ const jqueryModal = () => {
       }
       totalSumSpan.empty().append(totalSum);
     });
-
-    $('#video-modal').on($.modal.OPEN, function (event, modal) {
-      var video = this.querySelector('iframe[src*="www.youtube.com"], iframe[src*="player.vimeo.com"], video');
-      console.log(video);
-      if (!video) return; // If an HTML5 video, play it
-      if (video.tagName.toLowerCase() === 'video') {
-        video.play();
-        return;
-      }
-      video.src = video.src + (video.src.indexOf('?') < 0 ? '?' : '&') + 'autoplay=1';
-    });
-    $('#video-modal').on($.modal.BEFORE_CLOSE, function (event, modal) {
-      var video = this.querySelector('iframe[src*="www.youtube.com"], iframe[src*="player.vimeo.com"], video');
-      if (!video) return;
-      if (video.tagName.toLowerCase() === 'video') {
-        video.pause();
-        return;
-      } // Remove autoplay from video src
-      video.src = video.src.replace('&autoplay=1', '').replace('?autoplay=1', '');
-    });
   };
 
   return {
