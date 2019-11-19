31b6bc761a297f6ccabaedc993a5f59826974b4d
artem.kalantai <artem.kalantai@centro.team>
2019-11-19 13:48:45 +0200

    ajax-loader copy

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ echo "copying from locales/ to ../public_html/"
 sudo cp -r locales/ ../public_html/
 echo "copying index.html to ../public_html/"
 sudo cp index.html ../public_html/
-echo "copying from node_modules/slick-carousel/slick/ajax-loader.gif to assets/css/"
+echo "copying from node_modules/slick-carousel/slick/ajax-loader.gif to ../public_html/assets/css/"
 sudo cp node_modules/slick-carousel/slick/ajax-loader.gif assets/css/
