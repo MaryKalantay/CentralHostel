1a2d61f77d9a4d05f384dd6671d35ca6dd6e0bef
root <root@mkarapuz.com.ua>
2020-03-12 09:17:07 +0000

    Changed deploy

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,22 @@
 git pull
 npm i
 gulp
-echo "Copying from assets/ to ../public_html/"
-sudo cp -r assets/ ../public_html/
-echo "Copying from locales/ to ../public_html/"
-sudo cp -r locales/ ../public_html/
+echo "Copying from assets/ to ../hostel.kharkiv.ua/"
+sudo cp -r assets/ ../hostel.kharkiv.ua/
+echo "Copying from locales/ to ../hostel.kharkiv.ua/"
+sudo cp -r locales/ ../hostel.kharkiv.ua/
 echo "Removing index.html files for all languages"
-sudo find ../public_html/ -maxdepth 1 -type f -name 'index*.html' -exec rm -- '{}' +
-echo "Copying index.html to ../public_html/"
-sudo cp index.html ../public_html/index_nolang.html
-echo "Copying sitemap.xml to ../public_html/"
-sudo cp sitemap.xml ../public_html/
-echo "Copying index.php to ../public_html/"
-sudo cp index.php ../public_html/
-echo "Copying from node_modules/slick-carousel/slick/ajax-loader.gif to ../public_html/assets/css/"
-sudo cp node_modules/slick-carousel/slick/ajax-loader.gif ../public_html/assets/css/
-sudo mkdir ../public_html/assets/css/fonts
-sudo cp node_modules/slick-carousel/slick/fonts/slick.ttf ../public_html/assets/css/fonts/
-sudo cp node_modules/slick-carousel/slick/fonts/slick.woff ../public_html/assets/css/fonts/
+sudo find ../hostel.kharkiv.ua/ -maxdepth 1 -type f -name 'index*.html' -exec rm -- '{}' +
+echo "Copying index.html to ../hostel.kharkiv.ua/"
+sudo cp index.html ../hostel.kharkiv.ua/index_nolang.html
+echo "Copying sitemap.xml to ../hostel.kharkiv.ua/"
+sudo cp sitemap.xml ../hostel.kharkiv.ua/
+echo "Copying index.php to ../hostel.kharkiv.ua/"
+sudo cp index.php ../hostel.kharkiv.ua/
+echo "Copying from node_modules/slick-carousel/slick/ajax-loader.gif to ../hostel.kharkiv.ua/assets/css/"
+sudo cp node_modules/slick-carousel/slick/ajax-loader.gif ../hostel.kharkiv.ua/assets/css/
+sudo mkdir ../hostel.kharkiv.ua/assets/css/fonts
+sudo cp node_modules/slick-carousel/slick/fonts/slick.ttf ../hostel.kharkiv.ua/assets/css/fonts/
+sudo cp node_modules/slick-carousel/slick/fonts/slick.woff ../hostel.kharkiv.ua/assets/css/fonts/
 npm install uglify-js -g
-uglifyjs --compress --mangle --output ../public_html/assets/js/app.min.js -- ../public_html/assets/js/app.js
+uglifyjs --compress --mangle --output ../hostel.kharkiv.ua/assets/js/app.min.js -- ../hostel.kharkiv.ua/assets/js/app.js
